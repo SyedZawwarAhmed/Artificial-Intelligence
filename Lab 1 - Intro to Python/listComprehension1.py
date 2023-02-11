d970642a9f3a75a4b27b36245f58fbca690b8fb2
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-02-11 16:58:28 +0500

    update listComprehension1.py

--- Lab 1 - Intro to Python/listComprehension1.py
+++ Lab 1 - Intro to Python/listComprehension1.py
@@ -1,10 +1,6 @@
 # Write a list comprehension which, from a list, generates a lowercased version of each string that has length greater than five.
 
 inputList =  ['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow', 'Teapink']
-outputList = []
+outputList = [string for string in inputList if len(string) > 5]
 
-for string in inputList:
-    if len(string) > 5:
-        outputList.append(string.lower())
-
-print(outputList) 
+print(outputList)
