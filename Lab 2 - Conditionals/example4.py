cc52d8e875e7bc86ac03b664212d354946ebde41
SyedZawwarAhmed <zawwar.ahmed12@gmail.com>
2023-02-11 18:10:59 +0500

    feat: create example4.py

---
+++ Lab 2 - Conditionals/example4.py
@@ -0,0 +1,5 @@
+a = int (input ("Enter a number between 10-20 or 30-40: "))
+if (a > 10 and a <= 20) or (a >= 30 and a <= 40) :
+    print ("The condition has been met.")
+else:
+    print ("You did it wrong.")
